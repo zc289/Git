ba78c64081a4a5ebc9cc2f15092b71f20c466120
Zhuoyang Cheng <zc289@exeter.ac.uk>
2016-11-15 14:52:56 +0000

    Revert "Change of variable"
    This reverts commit eb702c9fc26d47145b4ea405b60bf2da62407c09.

--- R.c
+++ R.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, sumOfTwoNums;
+    int n1, n2, Sum;
     
     printf("Enter two integers: ");
 
@@ -9,15 +9,15 @@ int main()
     scanf("%d %d", &n1, &n2);
 
     // store sum in variable Sum
-    sumOfTwoNums = n1 + n2;
+    Sum = n1 + n2;
 
     // display output     
-    printf("%d + %d = %d\n", n1, n2, sumOfTwoNums);
+    printf("%d + %d = %d\n", n1, n2, Sum);
     
     // check if the sum is even or odd, 
     // and print result to screen
-    if(sumOfTwoNums % 2 == 0) printf("%d is even\n", sumOfTwoNums);
-    else printf("%d is odd\n", sumOfTwoNums);
+    if(Sum % 2 == 0) printf("%d is even\n", Sum);
+    else printf("%d is odd\n", Sum);
 
     return 0;
 }
