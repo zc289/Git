bca5a891a7a500348911a80bab1e12026414afae
Zhuoyang Cheng <zc289@exeter.ac.uk>
2016-11-15 15:12:45 +0000

    new line

--- R.c
+++ R.c
@@ -10,10 +10,10 @@ int main()
 
 	if(n1>n2)
 	{
-		prinft("%d %d",n1,n2);
+		printf("%d %d\n",n1,n2);
 	}
 	else
-		printf("%d %d",n2,n1);
+		printf("%d %d\n",n2,n1);
     // store sum in variable Sum
     Sum = n1 + n2;
 
